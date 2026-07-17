8bc5c384bc458d50ceb269787fcdfff532f667f4
smrp-martapura <settlemartapura@gmail.com>
2026-07-17 19:39:46 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,7 @@
-const CACHE_NAME = 'tiga-pusako-v1';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-// Install Service Worker
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-// Fetch Assets
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
